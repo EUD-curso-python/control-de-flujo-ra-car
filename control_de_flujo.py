b31ef0781e6442352c01a4140a4aa9b09a435daf
Rafael Cardenas <>
2021-04-11 04:04:41 +0000

    .

--- control_de_flujo.py
+++ control_de_flujo.py
@@ -95,8 +95,7 @@ for x in range(51):
 `invertido` (sin hacer uso de la función `reversed` ni del método `reverse`)
 """
 lista2 = list(range(1, 70, 5))
-print(list(range(1, 70, 5)))
-print(list(range(70, 1, -5)))
+Invierta = lista2[::-1]
 
 
 
@@ -104,8 +103,15 @@ print(list(range(70, 1, -5)))
 Nota: Un número primo es un número entero que no se puede calcular multiplicando 
 otros números enteros.
 """
-
-
+primos = []
+primo = True
+for x in range(37,300):
+  for n in range(2,x):
+    if(x%n == 0):
+      primo = False
+  if(primo == True):
+    primos.append(x)
+  primo = True
 
 
 
@@ -117,7 +123,14 @@ del segundo cada uno se calcula sumando los dos anteriores términos de la serie
 [0, 1, 1, 2, 3, 5, 8, ...]
 
 """
-
+fibonacci = [0,1]
+n1 = 0
+n2 = 1
+for x in range(2,60):
+  n3 = n1+ n2
+  fibonacci.append(n3)
+  n1 = n2
+  n2 = n3
 
 
 
@@ -130,7 +143,9 @@ Por ejemplo, el factorial de 5 se calcula así:
 
 5! = 5 × 4 × 3 × 2 × 1 = 120
 """
-
+factorial = 1
+for x in range(2,31):
+  factorial = factorial* x
 
 
 
@@ -141,7 +156,12 @@ presentes en posiciones pares, pero solo hasta la posición 80.
 
 lista3 = [941, 149, 672, 208, 99, 562, 749, 947, 251, 750, 889, 596, 836, 742, 512, 19, 674, 142, 272, 773, 859, 598, 898, 930, 119, 107, 798, 447, 348, 402, 33, 678, 460, 144, 168, 290, 929, 254, 233, 563, 48, 249, 890, 871, 484, 265, 831, 694, 366, 499, 271, 123, 870, 986, 449, 894, 347, 346, 519, 969, 242, 57, 985, 250, 490, 93, 999, 373, 355, 466, 416, 937, 214, 707, 834, 126, 698, 268, 217, 406, 334, 285, 429, 130, 393, 396, 936, 572, 688, 765, 404, 970, 159, 98, 545, 412, 629, 361, 70, 602]
 
-
+pos = 0
+pares = []
+for x in lista3:
+  if(pos%2 == 0 and pos<= 80):
+    pares.append(x)
+  pos = pos +1  
 
 
 
@@ -149,7 +169,10 @@ lista3 = [941, 149, 672, 208, 99, 562, 749, 947, 251, 750, 889, 596, 836, 742, 5
 """Guarde en lista `cubos` el cubo (potencia elevada a la 3) de los números del 
 1 al 100. 
 """
-
+cubos = []
+for x in range(1,101):
+  num = x*x*x
+  cubos.append(num)
 
 
 
@@ -157,7 +180,11 @@ lista3 = [941, 149, 672, 208, 99, 562, 749, 947, 251, 750, 889, 596, 836, 742, 5
 """Encuentre la suma de la serie 2 +22 + 222 + 2222 + .. hasta sumar 10 términos 
 y guardar resultado en variable `suma_2s` 
 """
-
+suma_2s= 0
+for x in range(0, 11):
+    t = 10 ** x * (10 - x) * 2
+    suma_2s = t + suma_2s
+print(suma_2s)
 
 
 
@@ -183,6 +210,10 @@ cantidad máxima de asteriscos de 30.
 *
 """
 
-
-
-
+patron = '*'
+for x in range(2,31):
+    patron = patron + "\n"
+    patron = patron + ("*"*x)
+for x in range(29,0,-1):
+    patron = patron + "\n"
+    patron = patron + ("*"*x)
